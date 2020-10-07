4cdd9307db3110fd010836d67ac653322dc0f1f9
Natanael Martinez Morales <natanael29032000@gmail.com>
2020-10-07 13:58:12 +0100

    Project created

---
+++ src/kata1/Person.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+/**
+ *
+ * @author natanaelmartinezmorales
+ */
+public class Person {
+    private final String name;
+    
+    
+}
