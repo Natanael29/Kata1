7011c37e980512134ecddf753df9df46b730728e
Natanael Martinez Morales <natanael29032000@gmail.com>
2020-12-20 12:10:56 +0000

    Kata 1 - 2.0 Version

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,12 +1,17 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person("Blas", "Cabrera", new Date(49, 8, 24));
-        System.out.println(person.getName() + " " + person.getSurname() + " tiene " + person.getAge());
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1949, 9, 24);
+        
+        Person person = new Person("Blas", "Cabrera", date);
+        System.out.println(person.getName() + " " + person.getSurname() + " tiene " + person.getAge() + " años");
     }
     
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,13 +1,15 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
     private final String surname;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, String surname, Date birthdate) {
+    public Person(String name, String surname, Calendar birthdate) {
         this.name = name;
         this.surname = surname;
         this.birthdate = birthdate;
@@ -21,12 +23,17 @@ public class Person {
         return surname;
     }
 
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return birthdate;
     }
     
     public int getAge() {
-        return (int) ((new Date().getTime() - birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+    }
+    
+    private long milliSecondsToYear(long milles) {
+        return milles/MILLISECONDS_PER_YEAR;
     }
     
     @Override
