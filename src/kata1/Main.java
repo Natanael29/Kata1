5a8f6b0415bae4cd0bda4329e94adedd087ca25f
Natanael Martinez Morales <natanael29032000@gmail.com>
2020-12-20 12:25:41 +0000

    Kata 1 - 2.0 Version

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,14 +1,12 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1949, 9, 24);
+        LocalDate date;
+        date = LocalDate.of(1949, 9, 24);
         
         Person person = new Person("Blas", "Cabrera", date);
         System.out.println(person.getName() + " " + person.getSurname() + " tiene " + person.getAge() + " años");
